0e25236f20a25ec5c6e04201c74c2928fcc41038
Ali Raza <aaraza1995@gmail.com>
2024-11-17 12:55:55 -0800

    1.0.2: Import fix (#9)
    Version 1.0.1 had an import error. Should be tested locally before publishing to PyPi.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,6 +2,10 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -9,7 +13,7 @@
 project = 'Cricinfo'
 copyright = '2024, Ali Raza'
 author = 'Ali Raza'
-release = '1.0.1'
+release = '1.0.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -19,17 +23,13 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../'))
+
 
 extensions = [
     'sphinx.ext.autodoc',          # Automatically document modules
