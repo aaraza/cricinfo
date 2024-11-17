9147010de57665e6845cfdfab67a55f5285d3f6b
Ali Raza <aaraza1995@gmail.com>
2024-11-17 14:53:49 -0600

    linting fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,6 +2,10 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -19,17 +23,13 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-import os
-import sys
-sys.path.insert(0, os.path.abspath('../../'))
+
 
 extensions = [
     'sphinx.ext.autodoc',          # Automatically document modules
